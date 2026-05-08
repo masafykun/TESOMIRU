14e94002f1ac4108183d44fb669f7b7e4951c4b0
Masato Suzuki <masafy.masato@gmail.com>
2026-05-08 11:57:31 +0900

    Initial Commit

---
+++ TESOMIRU/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TESOMIRU
+//
+//  Created by 鈴木聖人 on 2026/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TESOMIRU/TESOMIRUApp.swift
@@ -0,0 +1,17 @@
+//
+//  TESOMIRUApp.swift
+//  TESOMIRU
+//
+//  Created by 鈴木聖人 on 2026/05/08.
+//
+
+import SwiftUI
+
+@main
+struct TESOMIRUApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
